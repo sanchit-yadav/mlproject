33e5daa6e49157d89d55a1f97266ee58ce7288d0
Sanchit Yadav <ysanchit78@gmail.com>
2025-08-19 16:41:59 +0530

    Again Model Traning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,7 +27,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv("notebook\data\stud.csv")
+            df = pd.read_csv(r"notebook\data\stud.csv")
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr  = data_transformation.initiate_data_transformation(train_data, test_data)
 
     modeltraner = ModelTrainer()
     print(modeltraner.initiate_model_trainer(train_arr, test_arr))
